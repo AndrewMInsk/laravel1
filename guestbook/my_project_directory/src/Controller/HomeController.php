bdcdf6a92b6f847fa957c5f3181382e70ab48edf
Andrew Zubarev <info@a-site.by>
2022-05-26 22:06:09 +0300

    11

--- guestbook/my_project_directory/src/Controller/HomeController.php
+++ guestbook/my_project_directory/src/Controller/HomeController.php
@@ -2,10 +2,11 @@
     namespace App\Controller;
 
 
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
-    class HomeController
+    class HomeController extends AbstractController
     {
         /**
          * @Route("/", name="homepage")
@@ -13,9 +14,11 @@
         public function number()
         {
             $number = random_int(0, 44);
-
+         //   dump($number);
             return new Response(
                 '<html><body>Lucky number: '.$number.'</body></html>'
             );
+            return $this->render('lucky/number.html.twig', ['number' => $number]);
+
         }
     }
