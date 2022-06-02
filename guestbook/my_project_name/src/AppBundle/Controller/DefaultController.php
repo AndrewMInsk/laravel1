a29db6242d20898b0e66b9f9dbd71ca0334947fb
Andrew Zubarev <info@a-site.by>
2022-06-02 10:32:44 +0300

    11

--- guestbook/my_project_name/src/AppBundle/Controller/DefaultController.php
+++ guestbook/my_project_name/src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@
 
     use AppBundle\Form\FeedbackType;
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,12 +20,26 @@
             return $this->render('@App/home.html.twig');
         }
 
+
         /**
          * @Route("/feedback", name="feedback")
+         * @param Request $request
+         * @return \Symfony\Component\HttpFoundation\Response|null
          */
-        public function feedbackAction()
+        public function feedbackAction(Request $request)
         {
             $form = $this->createForm(FeedbackType::class);
+            $form->add('submit', SubmitType::class);
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                $feedback=$form->getData();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($feedback);
+                $em->flush();
+                $this->addFlash('success1', 'saved');
+                return $this->redirectToRoute('feedback');
+            }
             // replace this example code with whatever you need
             return $this->render('@App/feedback.html.twig',['feedback_form'=>$form->createView()]);
         }
