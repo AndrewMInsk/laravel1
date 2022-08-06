5126028b06a5ea37bdea03e7cbcb7ed07ae3f05b
Andrew Zubarev <info@a-site.by>
2022-08-06 16:19:53 +0200

    start

--- test/index.php
+++ test/index.php
@@ -1,2 +1,7 @@
 <?php
-111
+
+    require_once __DIR__ . '/vendor/autoload.php';
+    $controller = new \src\Controller();
+    if (isset($_GET['action']) && !empty($_GET['action'])) {
+        $controller->{$_GET['action']}();
+    }
